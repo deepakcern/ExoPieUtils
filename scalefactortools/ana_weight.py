eb6c8b203f77d0acf49f2f4ec8c838c526a7afac
Praveen Chandra Tiwari <tiwariPC@users.noreply.github.com>
2019-09-10 00:35:34 +0530

    some changes in the sfs

--- scalefactortools/ana_weight.py
+++ scalefactortools/ana_weight.py
@@ -1,4 +1,19 @@
-import SFReader as SFR
+
+
+if era=='2016':
+    import SFReader_2016 as SFR
+    import EWKfactory_2016 as ewk
+
+elif era=='2017':
+    import SFReader_2017 as SFR
+    import EWKfactory_2017 as ewk
+
+elif era=='2018':
+    import SFReader_2018 as SFR
+    import EWKfactory_2018 as ewk
+else:
+    print("Please tell me which year\'s scale factors you would like to apply (2016,2017 or 2018)? ")
+
 
 def ele_weight(pt,eta,ID='None'):
     trig_w = SFR.getEleTrigSF(pt,eta)
@@ -33,3 +48,50 @@ def mu_weight(pt,eta,ID='None'):
     elif ID =='None':
         print ('Please select which ID muon you want(L or T)')
     return weight
+
+def getMETtrig_First(met):
+    return SFR.getMETtrig_First(met)
+
+def getMETtrig_Second(met):
+    return SFR.getMETtrig_Second(met)
+
+def puweight(pu):
+    return SFR.puweight(pu)
+
+def getEWKW(pt):
+    return ewk.getEWKW(pt)
+
+def getQCDW(pt):
+    return ewk.getQCDW(pt)
+
+def getRenUpW(pt):
+    return ewk.getRenUpW(pt)
+
+def getRenDownW(pt):
+    return ewk.getRenDownW(pt)
+
+def getFacUpW(pt):
+    return ewk.getFacUpW(pt)
+
+def getFacDownW(pt):
+    return ewk.getFacDownW(pt)
+
+def getEWKZ(pt):
+    return ewk.getEWKZ(pt)
+
+def getQCDZ(pt):
+    return ewk.getQCDZ(pt)
+
+def getRenUpZ(pt):
+    return ewk.getRenUpW(pt)
+
+def getRenDownZ(pt):
+    return ewk.getRenDownW(pt)
+
+def getFacUpZ(pt):
+    return ewk.getFacUpW(pt)
+
+def getFacDownZ(pt):
+    return ewk.getFacDownW(pt)
+
+    
