50a4e092ac4800edd63a99a020b6884015984076
Praveen Chandra Tiwari <physics.pctiwari@gmail.com>
2021-07-09 05:24:59 +0530

    adde few function

--- commonutils/MathUtils.py
+++ commonutils/MathUtils.py
@@ -1,5 +1,5 @@
-import os 
-import sys 
+import os
+import sys
 import optparse
 
 import numpy
@@ -7,9 +7,9 @@ import pandas
 import math
 from root_pandas import read_root
 from pandas import  DataFrame
-from pandas import Series 
+from pandas import Series
 from ROOT import TLorentzVector, TMath
-import numpy 
+import numpy
 
 def makep4(px, py,pz,e):
     p4=TLorentzVector(0.,0.,0.,0.)
@@ -37,9 +37,9 @@ def getPtEtaPhiP(px, py, pz, e):
     p_['py'] = py
     p_['pz'] = pz
     p_['e'] = e
-    
+
     print type(p_)
-    
+
     pt_=[]
     eta_=[]
     phi_=[]
@@ -50,7 +50,7 @@ def getPtEtaPhiP(px, py, pz, e):
         print "inside zip loop", row, row1
         #print p_[row[0]]
 
-    
+
     return [px_,px_,px_,px_]
 
     '''
@@ -61,15 +61,15 @@ def getPtEtaPhiP(px, py, pz, e):
         pz_ =   row['pz']
         e_  =   row['e']
         '''
-        ## list comprehension to compute pt 
+        ## list comprehension to compute pt
         pt  = [numpy.sqrt( px_[irow]**2 + py_[irow]**2 ) for irow in range(len(px_))]
         p   = Series([numpy.sqrt( px_[irow]**2 + py_[irow]**2 + pz_[irow]**2) for irow in range(len(px_))])
         eta = [numpy.log((p[irow] + pz_[irow])/(p[irow] - pz_[irow]))/2 for irow in range(len(pz_)) ]
         phi = [numpy.arctan2( py_[irow], px_[irow]) for irow in range(len(px_))]
-        
-        ## following code make the p4 using list comprehension but its slow. 
+
+        ## following code make the p4 using list comprehension but its slow.
         #pt = [makep4(row['px'][irow], row['py'][irow], row['pz'][irow], abs(row['e'][irow])).Pt() for irow in range(len(px_))]
-        
+
 
         pt_.append(pt)
         eta_.append(eta)
@@ -80,11 +80,11 @@ def getPtEtaPhiP(px, py, pz, e):
     p_["eta"]=Series(eta_)
     p_["phi"]=Series(phi_)
     p_["p3"]=Series(p3_)
-    
+
     print p_["phi"].head()
     '''
-    
-    
+
+
     return [pt_,eta,phi,p3_]
 
 
@@ -99,7 +99,7 @@ def getPt(px_, py_):
 def getEta(px_, py_, pz_):
     p3_   = getP3(px_, py_, pz_)
     return( numpy.log((p3_ + pz_)/(p3_ - pz_))/2)
-    
+
 def getPhi(px_, py_):
     return( numpy.arctan2( py_, px_))
 
@@ -107,7 +107,7 @@ def getPhi(px_, py_):
 def getPtEtaPhiPFast(px_, py_, pz_, e_):
     '''
     pt_  = numpy.sqrt( px_**2 + py_**2)
-    p3_   = numpy.sqrt( px_**2 + py_**2 + pz_**2) 
+    p3_   = numpy.sqrt( px_**2 + py_**2 + pz_**2)
     eta_ = numpy.log((p3_ + pz_)/(p3_ - pz_))/2
     phi_ = numpy.arctan2( py_, px_)
     '''
@@ -115,7 +115,7 @@ def getPtEtaPhiPFast(px_, py_, pz_, e_):
     pt_ = getPt(px_, py_)
     eta_ = getEta(px_, py_, pz_)
     phi_=getPhi(px_, py_)
-    
+
     return [pt_,eta_,phi_,p3_]
 
 
@@ -137,10 +137,10 @@ def Delta_R(eta1, eta2, phi1,phi2):
     deltaeta = eta1-eta2
     deltaphi = DeltaPhi(phi1,phi2)
     DR = TMath.Sqrt ( deltaeta**2 + deltaphi**2 )
-    return DR 
+    return DR
 
-## this function is not needed, just keeping for backward compatibility, 
-## p4 already have a function DeltaR 
+## this function is not needed, just keeping for backward compatibility,
+## p4 already have a function DeltaR
 
 def DeltaR(p4_1, p4_2):
     eta1 = p4_1.Eta()
@@ -162,7 +162,7 @@ def MT(Pt, met, dphi):
 
 
 def getMT(Pt, met, phi1, phi2):
-    
+
     dphi = DeltaPhi(phi1, phi2)
     return TMath.Sqrt( 2 * Pt * met * (1.0 - TMath.Cos(dphi)) )
 
@@ -182,3 +182,22 @@ def InvMass(px1,py1,pz1,pe1,px2,py2,pz2,pe2):
     return inv_mass
 
 
+def dijetPt(px1, py1, pz1, pe1, px2, py2, pz2, pe2):
+    p1 = TLorentzVector(px1, py1, pz1, pe1)
+    p2 = TLorentzVector(px2, py2, pz2, pe2)
+    dijet_pt = (p1+p2).Pt()
+    return dijet_pt
+
+
+def dijetPhi(px1, py1, pz1, pe1, px2, py2, pz2, pe2):
+    p1 = TLorentzVector(px1, py1, pz1, pe1)
+    p2 = TLorentzVector(px2, py2, pz2, pe2)
+    dijet_phi = (p1+p2).Phi()
+    return dijet_phi
+
+
+def dijetEta(px1, py1, pz1, pe1, px2, py2, pz2, pe2):
+    p1 = TLorentzVector(px1, py1, pz1, pe1)
+    p2 = TLorentzVector(px2, py2, pz2, pe2)
+    dijet_eta = (p1+p2).Eta()
+    return dijet_eta
